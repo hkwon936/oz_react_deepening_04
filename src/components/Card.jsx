dc89bfbd780cd1f00e2c2bc5dbdb7d2fcb1b9975
Heejeong Kwon <hkwon936@naver.com>
2025-11-05 20:43:30 +0900

    도전과제1: 카드 갤러리 완성하기

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -7,12 +7,16 @@ export default function Card({ item }) {
     return (
         <div className="card">
             <div className="card-image">
-                <img src={`https://placehold.co/600x400`} />
+                { /* 카드 이미지 */}
+                <img src="https://placehold.co/600x400" alt={item.title} />
             </div>
             <div className="card-content">
-                <span className="card-category">???</span>
-                <h2 className="card-title">???</h2>
-                <p className="card-description">???</p>
+                { /* 카테고리 */}
+                <span className="card-category">{item.category}</span>
+                { /* 제목 */}
+                <h2 className="card-title">{item.title}</h2>
+                { /* 설명 */}
+                <p className="card-description">{item.description}</p>
             </div>
         </div>
     );
